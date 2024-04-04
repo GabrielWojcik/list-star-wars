7a3ac7b8d237b450780ae72892839943c16c2871
GabrielWojcik <gabrielwojcikv@gmail.com>
2024-04-04 13:54:09 -0300

    fix: fixed reports for testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,23 @@
 import { CardCharacter } from "@/components/home";
+import NavBar from "@/components/navBar";
 export default async function Initial() {
 
   return (
+    <>
+    <NavBar />
     <div className="flex flex-col gap-5 items-center justify-center h-screen w-screen  bg-black text-yellow-400">
-      <div className="">
-        <strong className="text-3xl">
-          Star Wars Characters
-        </strong>
-      </div>
+        <header>
+          <h1 className="text-3xl">
+            Star Wars Characters
+          </h1>
+        </header>
       <div className="flex flex-wrap items-center justify-center h-3/6 bg-black">
+      <main>
           <CardCharacter />
+      </main>
       </div>
     </div>
-  );
+    </>
+   );
 }
 
